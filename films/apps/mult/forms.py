9173aaea711bef09b4592fde2b18104cf8ac301b
howe251 <misha.sosnin245@gmail.com>
2021-12-13 22:16:40 +0300

    ночной дизайн,сортировка на страницах

--- films/apps/mult/forms.py
+++ films/apps/mult/forms.py
@@ -3,13 +3,9 @@ from .models import Mult
 
 
 class SortForm(forms.Form):
-    # az = forms.ChoiceField(label="По алфавиту", required=False)
     ordering = forms.ChoiceField(label="сортировка", required=False, initial=["-name", "По убыванию"], choices=[
         ["name", "По алфавиту А-Я"],
         ["-name", "По алфавиту Я-А"],
         ["create_date", "Сначала новые"],
         ["-create_date", "Сначала старые"]
     ])
-    # ordering = ModelForm.ChoiceField(label="Сортировка", required=False, choices=[
-    #
-    # ])
